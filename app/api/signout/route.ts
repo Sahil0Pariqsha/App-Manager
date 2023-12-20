f26667dcb3491ed2428af1c95545f8fb20f1c82e
Sahil0Pariqsha <sahil@pariqsha.com>
2023-12-20 13:17:49 +0530

    Fix: Error occurred prerendering page /api/signout

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,10 +1,17 @@
 import { cookies } from "next/headers";
 
-export const GET = () => {
+export const GET = (request: any) => {
   try {
+    const auth = request.cookies.get("userToken") || "";
+    if (!auth) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
     const response: any = {
       message: "User logged out successfully",
     };
+
     cookies().delete("userToken");
     return new Response(JSON.stringify(response), {
       status: 200,
